6fc9ce3d1c4a486026813cbd2192c4c4d5871892
Alexey Rivkind <alexey.rivkind@life.com.by>
2019-12-21 23:27:02 +0300

    AutoCompressor

---
+++ AutoCompressor/index.js
@@ -0,0 +1,49 @@
+const fs = require('fs').promises;
+var async = require('async');
+const fsSync = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+
+const folder = process.argv[2];
+const currentTab = process.argv[3];
+
+
+const folderPath = (currentTab && currentTab === 'current')? path.join(__dirname, folder) : folder;
+
+const readDirectory = async (dirPath) => {
+    try {
+        const files = await fs.readdir(dirPath);
+
+        async.eachSeries(files, async file => {
+            if(!(/gz$/.test(file))){
+                const filePath = path.join(dirPath, file);
+                const fileInFolder = await fs.stat(filePath);
+                if (fileInFolder.isDirectory()) {
+                    console.log('Directory: ',filePath);
+                    await readDirectory(filePath);
+                } else if(fileInFolder.isFile()) {
+                    console.log('File: ',filePath);
+                    const gzipFN = filePath + '.gz';
+                    try {
+                        const gzipFile = await fs.stat(gzipFN);
+                        if(fileInFolder.mtime > gzipFile.mtime) {
+                            createGzip(filePath,gzipFN);
+                        }
+                    }catch (e) {
+                        createGzip(filePath,gzipFN);
+                    }
+                }
+            }
+        })
+    }catch (e) {
+        console.log('Directory not found')
+    }
+}
+
+const createGzip = async (originFile, gzFile) => {
+    const inp = fsSync.createReadStream(originFile);
+    const out = fsSync.createWriteStream(gzFile);
+    await inp.pipe(zlib.createGzip()).pipe(out);
+}
+
+readDirectory(folderPath);
